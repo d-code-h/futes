f2b699c0d23cc172e40fcfb7caa9551728ab92ce
Yunus Habeeb <habeebdh1@gmail.com>
2025-04-13 14:02:30 +0100

    refactor: add new pages for various sections; implement Hero and Footer components for consistent layout

---
+++ app/information/more/faqs/page.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+      <Hero title="FAQs" />
+      <main className="min-h-64 block"></main>
+      <Footer />
+    </>
+  );
+};
+
+export default page;
